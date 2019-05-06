adc576880252216715883c3a5975defbfa7ba63b
Ben Cooke <ben.cooke@arm.com>
2019-05-06 15:50:41 -0500

    Update PDMC to 3.1.0

--- pal_config_MbedOS.h
+++ pal_config_MbedOS.h
@@ -19,10 +19,10 @@
 
 #define PAL_USE_HW_ROT 0
 #define PAL_USE_HW_RTC 0
-#define PAL_USE_HW_TRNG 0
+#define PAL_USE_HW_TRNG 1
 #define PAL_SIMULATOR_FLASH_OVER_FILE_SYSTEM 0
 #define PAL_USE_INTERNAL_FLASH 0
-#define PAL_USE_SECURE_TIME 0
+#define PAL_USE_SECURE_TIME 1
 
 
 #include "mbedOS_SST.h"
